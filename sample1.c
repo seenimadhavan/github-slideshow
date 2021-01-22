5fe7ed9ef7615bc6e9f927803bc2e38fdf03d72c
seenimadhavan <77767873+seenimadhavan@users.noreply.github.com>
2021-01-22 10:04:07 +0530

    Create sample1.c
    first c program

---
+++ sample1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<sys/utsname.h>
+
+void main()
+{
+	struct utsname buf;
+	errno=0;
+	if(uname(&buf)!=0)
+	{
+		printf("Not return zero so error\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("System name : %s\n",buf.sysname);
+	printf("Node name : %s\n",buf.nodename);
+	printf("Release name : %s\n",buf.release);
+	printf("Version name : %s\n",buf.version);
+	printf("Machine name : %s\n",buf.machine);
+}
